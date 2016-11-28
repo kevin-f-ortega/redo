75dde0c50cdb7b97fddec40dc3ec4dd042d675bc
Robert L. Bocchino Jr <bocchino@icloud.com>
2016-11-27 16:24:20 -0800

    Remove installer test
    This doesn't really work with the new installation procedure.
    Instead, the test is to install redo and see if it works.
    
    This fixes a big that was interfering with the test runs
    ('redo test' was installing redo while it was running).

--- defs-root.sh
+++ defs-root.sh
@@ -2,6 +2,7 @@
 
 # ----------------------------------------------------------------------
 # defs-root.sh
+# Read definitions at the root of the build tree
 # ----------------------------------------------------------------------
 
 # Canonicalize path names
@@ -10,12 +11,12 @@ canonicalize()
   ( cd $1; pwd )
 }
 
+# Canonicalize REDO_ROOT
 export REDO_ROOT=`canonicalize $REDO_ROOT`
 
+# Find and source definition files
 files=$REDO_ROOT/defs/*.sh
-
 redo-ifchange $DIR/defs.sh $files
-
 for file in $files
 do
   . $file
